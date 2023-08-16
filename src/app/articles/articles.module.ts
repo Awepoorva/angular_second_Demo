09975e49d0b94502b51298e4a9e0b6488be7a479
Awepoorva <apurvac329@gmail.com>
2023-08-16 12:09:15 +0530

    something

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -3,11 +3,17 @@ import { CommonModule } from '@angular/common';
 
 import { ArticlesRoutingModule } from './articles-routing.module';
 import { HomeComponent } from './home/home.component';
+import { ViewArticlesComponent } from './view-articles/view-articles.component';
+import { CreateArticlesComponent } from './create-articles/create-articles.component';
+import { ListtitleComponent } from './listtitle/listtitle.component';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ViewArticlesComponent,
+    CreateArticlesComponent,
+    ListtitleComponent
   ],
   imports: [
     CommonModule,
