98037878e7cdfd886a0d4a2a5769fd3e54e12fe9
Awepoorva <apurvac329@gmail.com>
2023-08-16 12:32:53 +0530

    something

--- src/app/articles/articles-routing.module.ts
+++ src/app/articles/articles-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { ListtitleComponent } from './listtitle/listtitle.component';
+import { ViewArticlesComponent } from './view-articles/view-articles.component';
+import { CreateArticlesComponent } from './create-articles/create-articles.component';
 
-const routes: Routes = [{ path: 'articles/home', component: HomeComponent }];
+const routes: Routes = [{ path: 'articles/home', component: HomeComponent },
+{ path: '/articles/createArticle', component: CreateArticlesComponent },
+{ path: '/articles/viewArticle', component: ViewArticlesComponent },
+{ path: '/articles/listTitle', component: ListtitleComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
